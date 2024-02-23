5e7061f1836cbd9cf44fee368676e8d6980772e1
fbourguignon <franklin_bourguignon@hotmail.com>
2024-02-22 22:40:48 -0300

    Adicionando novos testes para validar o payload

--- src/test/java/br/com/rinhabackend/controller/ClientControllerTest.java
+++ src/test/java/br/com/rinhabackend/controller/ClientControllerTest.java
@@ -1,6 +1,5 @@
 package br.com.rinhabackend.controller;
 
-
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
@@ -8,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+
+import static org.codehaus.groovy.runtime.InvokerHelper.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 @MicronautTest
@@ -30,6 +31,26 @@ public class ClientControllerTest {
                 .post("/clientes/1/transacoes")
                 .then()
                 .statusCode(400)
-                .body("message", equalTo("Convite nao localizado para esse usuario"));
+                .body("errors", equalTo(asList("Tipo de transacao invalido")));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Deve retornar um erro ao informar um tipo de transacao invalido")
+    void create_transaction_invalid_description(RequestSpecification spec) {
+        spec
+                .when()
+                .body("""
+                        {
+                           "valor": 1000,
+                           "tipo": "d",
+                           "descricao": "descricao maior que 10"
+                        }
+                        """)
+                .contentType(ContentType.JSON)
+                .post("/clientes/1/transacoes")
+                .then()
+                .statusCode(400)
+                .body("errors", equalTo(asList("O tamanho maximo da descricao e 10 caracteres")));
     }
 }
